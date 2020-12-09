09dd69d631aa072c0ace9559540c5bada2a5700c
Yukee-798 <909397116@qq.com>
2020-12-09 18:48:13 +0800

    12-9-2020 AJAX

--- study03/1. Ajax/5. 原生AJAX/server.js
+++ study03/1. Ajax/5. 原生AJAX/server.js
@@ -18,11 +18,36 @@ app.get('/server', (request, response) => {
     response.send(`<button>GET请求，出现按钮!</button>`);
 });
 
-app.post('/server', (request, response) => {
+// 接受POST请求
+// app.post('/server', (request, response) => {
+//     // 设置响应头，设置允许跨域
+//     response.setHeader("Access-Control-Allow-Origin", "*");
+//     response.setHeader("Access-Control-Allow-Headers", "*" );
+//     // 设置响应响应体
+//     response.send(`<button>POST请求，出现按钮!</button>`);
+// });
+
+// 可以接受任何类型请求
+app.all('/server', (request, response) => {
     // 设置响应头，设置允许跨域
-    response.setHeader("Access-Control-Allow-Origin", '*');
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
     // 设置响应响应体
     response.send(`<button>POST请求，出现按钮!</button>`);
+});
+
+
+app.all('/server-json', (request, response) => {
+    // 设置响应头，设置允许跨域
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+    // 设置响应响应体，将结果返回给客户端
+    // 传递一个对象过去
+    const person = {
+        name: 'momo',
+        gender: 0
+    };
+    response.send(JSON.stringify(person));
 });
 
 // 4. 监听端口启动服务 
